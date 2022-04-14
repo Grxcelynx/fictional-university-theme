6f5beff800440cf15b1f9ac06f64353f38581e98
Grace Birnam <gbirnam@alphaworks.tech>
2022-04-14 10:58:45 -0700

    lesson 3.8 functions

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-This is our hella amazing theme 
+<?php
+
+    function greet($name, $color) {
+        echo "<p> Hi my name is $name and my fave color is $color </p>";
+    }
+
+    greet('Grace', 'Green');
+    greet('Channing', 'Lavendar');
+
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description');?></p>
