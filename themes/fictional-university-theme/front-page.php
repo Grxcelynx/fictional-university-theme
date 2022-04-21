928b42a4c685b55a026c281e76ae4de1331ff490
Grace Birnam <gbirnam@alphaworks.tech>
2022-04-21 14:15:49 -0700

    lesson 8 done

--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -10,7 +10,7 @@
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program') ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
 
@@ -42,10 +42,10 @@
             while($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
                           <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month"><?php 
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate->format('M');
+                 <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month"><?php 
+                  $eventDate = new DateTime(get_field('event_date'));
+                  echo $eventDate->format('M');
               
               ?></span>
               <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
